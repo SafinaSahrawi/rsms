1b0e3ab2f167b3ec5d83c57c4a4c9393615f603c
SafinaSahrawi <75608213+SafinaSahrawi@users.noreply.github.com>
2021-05-08 17:49:03 +0800

    Table Service Created
    php artisan make:model services -m

---
+++ database/migrations/2021_05_08_093658_create_services_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('deviceType');
+            $table->string('brand');
+            $table->string('serialNo');
+            $table->text('faulty');
+            $table->double('cost');
+            $table->string('status');
+            $table->timestamp('created_at')->date_create();
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,24 +13,19 @@ class ComposerAutoloaderInit8a4dc16ebd99347a870c295867cd63e0
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit8a4dc16ebd99347a870c295867cd63e0', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit8a4dc16ebd99347a870c295867cd63e0', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit8a4dc16ebd99347a870c295867cd63e0::getInitializer($loader));
         } else {
