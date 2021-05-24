542c57b26801a2999923ddc8f7aa592f230e9196
SafinaSahrawi <75608213+SafinaSahrawi@users.noreply.github.com>
2021-05-24 17:08:06 +0800

    sql update Safina

--- rsms.sql
+++ rsms.sql
@@ -32,14 +32,16 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table rsms.migrations: ~2 rows (approximately)
+-- Dumping data for table rsms.migrations: ~3 rows (approximately)
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(1, '2014_10_12_000000_create_users_table', 1),
 	(2, '2014_10_12_100000_create_password_resets_table', 1),
-	(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(4, '2021_05_08_093658_create_services_table', 2),
+	(5, '2021_05_23_201450_create_services_table', 3);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 
 -- Dumping structure for table rsms.password_resets
@@ -53,6 +55,27 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 -- Dumping data for table rsms.password_resets: ~0 rows (approximately)
 /*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
 /*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
+-- Dumping structure for table rsms.services
+CREATE TABLE IF NOT EXISTS `services` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `deviceType` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `serialNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `faulty` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cost` double NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table rsms.services: ~1 rows (approximately)
+/*!40000 ALTER TABLE `services` DISABLE KEYS */;
+INSERT INTO `services` (`id`, `deviceType`, `brand`, `serialNo`, `faulty`, `cost`, `status`, `created_at`, `updated_at`) VALUES
+	(1, 'laptop', 'hp', 'ab1234', 'motherboard', 123, NULL, '2021-05-23 20:45:03', '2021-05-23 20:45:03'),
+	(2, 'laptop', 'acer', '123', 'screen', 123, NULL, '2021-05-24 09:04:05', '2021-05-24 09:04:05');
+/*!40000 ALTER TABLE `services` ENABLE KEYS */;
 
 -- Dumping structure for table rsms.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -68,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table rsms.users: ~1 rows (approximately)
+-- Dumping data for table rsms.users: ~0 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 	(1, 'Safinatunnajah Binti Sahrawi', 'safinasahrawidns@gmail.com', NULL, '$2y$10$Xd6C.Z.Jt6KfUaqu3kv9J.tawAhVtJ9DliLBA0ehvBz8C/0vFzKhq', NULL, '2021-05-04 01:45:13', '2021-05-04 01:45:13'),
